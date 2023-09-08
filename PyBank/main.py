6842a8421707750c69569f338586ced33d6ce8e0
HollaNotes <noellewidick@gmail.com>
2023-09-08 15:43:19 -0700

    made adjustments to the print portion

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,6 +8,17 @@ import csv
 budget_data_csv = os.path.join('..', 'PyBank', 'Resources', 'budget_data.csv')
 print(f'budget_data_csv: {budget_data_csv}')
 
+# Define funtions
+
+
+# Set Variables
+dates = []
+profit_loss = []
+total_months = 0
+pl_total_net = 0
+
+
+
 with open(budget_data_csv) as csvfile:
 #   CSV reader specifies delimiter     
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -15,23 +26,53 @@ with open(budget_data_csv) as csvfile:
 
 #   Read the header row first
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
     
 
-#------PRINT CSV ROWS---------------------------------------------------
+#------PRINT CSV ROWS----------------------------------------------------------------
 #   Read each row of data after the header    
     #for row in csvreader:
         #print(row)    
-#-----------------------------------------------------------------------        
-
-#------TOTAL MONTHS-----------------------------------------------------
+#------------------------------------------------------------------------------------ 
+#        
+# 1 ------TOTAL MONTHS IN DATASET----------------------------------------------------
     # Count begins at zero and needs to increase by 1 as it counts the rows
-    count = 0
+      
     for row in csvreader:
-        count += 1
-    print(f'Total Months: {count}')  
-#------------------------------------------------------------------------    
+        total_months += 1
+    
+# 1 ---------------------------------------------------------------------------------  
+#   
+# 2 ------NET TOTAL AMOUNT OF PROFIT/LOSS OVER ENTIRE PERIOD-------------------------
+    # Need to get sum of entire column as it goes through the rows   
+    
+    
 
-#------TOTAL-------------------------------------------------------------
+# 2 ---------------------------------------------------------------------------------
+
+# 3 ------CHANGES IN PROFIT/LOSS OVER ENTIRE PERIOD, AND AVERAGE OF CHANGES----------
 
 
+# 3 ---------------------------------------------------------------------------------
+
+# 4 ------GREATEST INCREASE IN PROFITS (DATE AND AMOUNT) OVER ENTIRE PERIOD----------
+
+
+# 4 ---------------------------------------------------------------------------------
+
+# 5 ------GREATEST DECREASE IN PROFITS (DATE AND AMOUNT) OVER ENTIRE PERIOD----------
+
+
+# 5 ---------------------------------------------------------------------------------
+
+# 6 ------PRINT ALL------------------------------------------------------------------
+    print(" ")
+    print(f"Financial Analysis")
+    print(f"-----------------------------------------")
+    print(f"Total Months: {total_months}")
+    print(f"Total:   ")
+    print(f"Average Change:   ")
+    print(f"Greatest Increase in Profits:   ")
+    print(f"Greatest Decrease in Profits:   ")
+
+# 6 ---------------------------------------------------------------------------------
